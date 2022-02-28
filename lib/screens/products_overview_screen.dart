516ab0f84a6503c3b4f7804c8993975293ebfd6e
marco <marcowetter@gmx.de>
2022-02-28 10:27:24 +0100

    Added products screen.

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../widgets/app_drawer.dart';
+import '../providers/cart.dart';
+import '../screens/cart_screen.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text(
+                  'Display favorites',
+                ),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text(
+                  'Show all',
+                ),
+                value: FilterOptions.All,
+              ),
+            ],
+            icon: Icon(
+              Icons.more_vert,
+            ),
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).colorScheme.onPrimary,
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
+  }
+}
