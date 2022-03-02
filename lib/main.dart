74fe3f98455f98125938d7e4e124ad759ae738c9
marco <marcowetter@gmx.de>
2022-03-02 17:25:51 +0100

    Added user authentication with autologin, autologout and registration with firebase backend.

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,12 @@ import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
+import './screens/splash_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,33 +22,55 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Products>(
           // value: Products(),
-          create: (ctx) => Products(), // provider class
+          update: (ctx, auth, previousProducts) => Products(
+              auth.token,
+              auth.userId,
+              previousProducts.items == null ? [] : previousProducts.items),
+          create: (ctx) => Products(null, null, []), // provider class
         ),
         ChangeNotifierProvider(
           // value: Cart(),
           create: (ctx) => Cart(), // provider class
         ),
-        ChangeNotifierProvider(
-          create: (ctx) => Orders(),
+        ChangeNotifierProxyProvider<Auth, Orders>(
+          update: (ctx, auth, previousOrders) => Orders(auth.token, auth.userId,
+              previousOrders.orders == null ? [] : previousOrders.orders),
+          create: (ctx) => Orders(null, null, []),
         ),
       ],
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-            // primarySwatch: Colors.purple,
-            // accentColor: null,
-            fontFamily: 'Lato',
-            colorScheme: ColorScheme.fromSwatch(
-                primarySwatch: Colors.purple, accentColor: Colors.deepOrange)),
-        home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen(),
-          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-          EditProductScreen.routeName: (ctx) => EditProductScreen(),
-        },
+      child: Consumer<Auth>(
+        builder: (ctx, authData, _) => MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+              // primarySwatch: Colors.purple,
+              // accentColor: null,
+              fontFamily: 'Lato',
+              colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: Colors.purple,
+                  accentColor: Colors.deepOrange)),
+          home: authData.isAuthenticated
+              ? ProductsOverviewScreen()
+              : FutureBuilder(
+                  future: authData.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
+          // : AuthScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
+            OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          },
+        ),
       ),
     );
   }
--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -24,19 +24,17 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoriteStatus() async {
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
     final url = Uri.parse(
-        'https://shopappdb-f7a87-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json');
+        'https://shopappdb-f7a87-default-rtdb.europe-west1.firebasedatabase.app/userFavorites/$userId/$id.json?auth=$token');
     try {
-      final response = await http.patch(
+      final response = await http.put(
         url,
         body: json.encode(
-          {
-            'isFavorite': isFavorite,
-          },
+          isFavorite,
         ),
       );
       if (response.statusCode >= 400) {
